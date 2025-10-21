0c9dcadb49b4db9eac16770916a0df5c1f1101bc
João Victor <joaovictors2010@gmail.com>
2025-10-21 00:02:43 -0300

    tirando openfeign

--- crud-service/src/main/java/com/imd/crud_service/CrudServiceApplication.java
+++ crud-service/src/main/java/com/imd/crud_service/CrudServiceApplication.java
@@ -3,11 +3,9 @@ package com.imd.crud_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class CrudServiceApplication {
 
     public static void main(String[] args) {
